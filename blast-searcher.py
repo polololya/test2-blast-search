21605e4998818f8d9d74b6e690540395847710a1
Ольга Полещук <36551175+polololya@users.noreply.github.com>
2018-06-02 22:31:01 +0300

    Add files via upload

--- blast-searcher.py
+++ blast-searcher.py
@@ -10,17 +10,17 @@ from Bio.Blast import NCBIXML
 import sys
 
 
-# In[29]:
+# In[34]:
 
 
-E_VALUE_THRESH = float(sys.argv[1])
+E_VALUE_THRESH = 0.04
 with open("small-test.fasta", "r") as handle:
     with open ('result-blast.fasta','w') as out:
         with open('blast_search.xml', 'w') as file:
             records = SeqIO.parse(handle, "fasta")
             for i in records:
                 result_handle = NCBIWWW.qblast("blastn", "nt", i.seq, format_type='XML', alignments = 1, descriptions = 1)
-                file.write(result_handle.read())
+                #file.write(result_handle.read())
                 blast_records = NCBIXML.parse(result_handle)
                 blast_record = next(blast_records)
                 for alignment in blast_record.alignments:
@@ -29,4 +29,5 @@ with open("small-test.fasta", "r") as handle:
                             title = alignment.title.split('|')
                             out.write('>'+title[4]+'\n'+str(i.seq)+'\n')
                             break
+                    break
 
